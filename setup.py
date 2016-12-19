8df018c47315c65c62777df5defd482e2f5216cf
Michael Armbruster <armbrust@fim.uni-passau.de>
2016-12-19 21:06:46 +0100

    Forgot to update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pthfree',
-    version='1.0',
+    version='1.1',
     url='https://github.com/maki-chan/pthfree',
     license='Public Domain',
     author='maki-chan',
